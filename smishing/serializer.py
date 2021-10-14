05ee496b304e568eeae797307e944127f86f3165
juu924 <juu924>
2021-10-14 23:34:29 +0900

    회원가입 서버 구현

--- smishing/serializer.py
+++ smishing/serializer.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
-from .models import Member, Message, Guide
+from .models import Member
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = ['member_id', 'member_name']
 
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
-
-class GuideSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Guide
-        fields = '__all__'
--- smishing/views.py
+++ smishing/views.py
@@ -1,18 +1,25 @@
+from django.http import JsonResponse
 from rest_framework import viewsets
-from .models import Guide, Member, Message
-from .serializer import GuideSerializer, MemberSerializer, MessageSerializer
+from rest_framework.parsers import JSONParser
+from .models import Member
+from .serializer import MemberSerializer
+import json
 
 # Create your views here.
-
-class GuideViewSet(viewsets.ModelViewSet):
-    queryset = Guide.objects.all()
-    serializer_class = GuideSerializer
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
+def member_register(request):
+
+    if request.method == 'POST':
+        print("리퀘스트 로그" + str(request.body))
+       #name = request.POST.get('member_name', '')
+        data = JSONParser().parse(request)
+        serializer = MemberSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
